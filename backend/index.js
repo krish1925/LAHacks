b8cc9556c24307ad0d70979318d3fcc67ce4d309
Krish Patel <krishhnpatel@gmail.com>
2023-04-22 16:10:59 -0700

    Backend connected to MongoDB server

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,101 @@
-const PORT = 8000
-const express = require('express')
-const {MongoClient} = require('mongodb')
-const uri = "mongodb+srv://krishhnpatel:Hello@123@cluster0.ndlbumc.mongodb.net/?retryWrites=true&w=majority"
-const app =express()
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
 
-app.get('/',(req,res)=> {
-    res.json("Hello to my app")
+const app = express();
+const PORT = 8000;
+const uri = 'mongodb+srv://krishhnpatel:Hello123@cluster0.ndlbumc.mongodb.net/?retryWrites=true&w=majority';
 
-} )
+// create a single MongoClient instance to reuse for all database operations
+const client = new MongoClient(uri);
 
-app.get('/signup',(req,res)=> {
-    res.json("Hello to my app")
+app.use(cors());
+app.use(express.json());
 
-} )
+app.get('/', (req, res) => {
+    res.json('hello')
+}) 
 
-app.post('/users',(req,res)=> {
+app.get('/feed', (req, res) => {
+    res.json('hello')
+}) 
+
+app.get('/signup', (req,res) => {
+    res.json('hello')
+})
+app.post('/users',async(req,res)=> {
+    res.json('hello')
+})
+
+
+app.post('/signup', async (req, res) => {
+  try {
+    const userData = req.body;
+    const db = client.db('app-data');
+    const users = db.collection('users');
+    const result = await users.insertOne(userData);
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/users',async(req,res)=> {
     const client = new MongoClient(uri)
-    res.json("Hello to my Application")
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+        const returnedUsers = await users.find().toArray()
+        res.send(returnedUsers)
+    } finally{
+        await client.close()
+    }
 
 } )
 
-app.get('/users',(req,res)=> {
-    const client = new MongoClient(uri)
-    res.json("Hello to my Applications")
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-} )
 
-app.listen(PORT, () => console.log("Server running on port " +PORT))
+// const {v4: uuidv4} = require('uuid')
+// const jwt = require('jsonwebtoken')
+// const bcrypt = require('bcrypt')
+// const cors = require('cors')
+
+// app.use(cors())
+// app.use(express.json())
+
+// app.get('/',(req,res)=> {
+//     res.json("Hello to my app")
+// } )
+
+// app.get('/signup',(req,res)=> {
+//     res.json("Hello to my app")
+
+// } )
+
+// app.post('/users',(req,res)=> {
+//     const client = new MongoClient(uri)
+//     res.json("Hello to my Application")
+
+// } )
+
+// app.get('/users',async(req,res)=> {
+//     const client = new MongoClient(uri)
+//     try {
+//         await client.connect()
+//         const database = client.db('app-data')
+//         const users = database.collection('users')
+//         const returnedusers = await users.toArray()
+//         res.send(returnedUsers)
+//     } finally{
+//         await client.close()
+//     }
+
+// } )
+
+// app.listen(PORT, () => console.log("Server running on port " +PORT))
 
 /*const PORT = 8000
 const express = require('express')
