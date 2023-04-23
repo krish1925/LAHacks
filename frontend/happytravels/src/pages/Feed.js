f172c016b4efebad719cecd1f39976039e762980
preyasigaur <preyasi4203@gmail.com>
2023-04-22 23:34:36 -0700

    minor changes to feed

--- frontend/happytravels/src/pages/Feed.js
+++ frontend/happytravels/src/pages/Feed.js
@@ -34,7 +34,7 @@ function Feed() {
       <div class = "post-card">
       {filteredPosts.map(post => (
         <div key={post._id} className="card">
-          <h2> <span style={{ fontWeight: 'bold', fontSize: '30px' }}>{post.formData.username}</span> {' • '} <span style={{ color: '#999' , fontSize: '20px'}}>{post.formData.location}</span> </h2>
+          <h2> <span style={{ fontWeight: 'bold', fontSize: '30px', textAlign: 'left' }}>{post.formData.username}</span> {' • '} <span style={{ color: '#999' , fontSize: '20px'}}>{post.formData.location}</span> </h2>
           <div class="content">
           <img src={post.formData.img} alt="post picture"  />
           <p><b></b> {post.formData.caption}</p>
