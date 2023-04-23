04b3bbf64412078d51e1af6bdfc80cf4838fe626
nemi desai <nemi@s-169-232-98-68.resnet.ucla.edu>
2023-04-22 17:45:39 -0700

    onboarding edits

--- frontend/happytravels/src/pages/OnBoarding.js
+++ frontend/happytravels/src/pages/OnBoarding.js
@@ -10,20 +10,13 @@ function OnBoarding() {
     // const [cookies, setCookie, removeCookie] = useCookies(['user'])
     const [formData, setFormData] = useState ({
         // user_id: cookies.UserId,
+        user_id: '',
         first_name: '',
         last_name: '',
-        dob_day: '',
-        dob_month: '',
-        dob_year: '',
-        show_gender: '',
-        gender_identity: '',
-        gender_interest: '',
+        pronouns: '',
         url: '',
         about: '',
-        matches: [],
-        workout_time:'',
-        workout_intensity:'',
-        favorite_exercise:''
+        password: ''
     })
 
     let navigate = useNavigate()
@@ -32,26 +25,26 @@ function OnBoarding() {
     const handleSubmit = async (e) => {
         e.preventDefault();
       
-        const isInvalidBirthday = formData.dob_day > 31 ||
-          formData.dob_month > 12 ||
-          formData.dob_year > 2023;
+        // const isInvalidBirthday = formData.dob_day > 31 ||
+        //   formData.dob_month > 12 ||
+        //   formData.dob_year > 2023;
       
-        const isFocusOnSchool = formData.dob_year > 2010;
+        // const isFocusOnSchool = formData.dob_year > 2010;
       
-        if (isInvalidBirthday) {
-          alert("Invalid birthday");
-          return; // early return to avoid clearing form data
-        }
+        // if (isInvalidBirthday) {
+        //   alert("Invalid birthday");
+        //   return; // early return to avoid clearing form data
+        // }
       
-        if (isFocusOnSchool) {
-          alert("Focus on school bro");
-          return; // early return to avoid clearing form data
-        }
+        // if (isFocusOnSchool) {
+        //   alert("Focus on school bro");
+        //   return; // early return to avoid clearing form data
+        // }
       
         console.log("submitted");
       
         try {
-          const response = await axios.put("http://localhost:8000/users", { formData });
+          const response = await axios.post("http://localhost:8000/users", { formData });
           const success = response.status === 200;
           if (success) navigate("/feed");
         } catch (err) {
@@ -111,99 +104,16 @@ function OnBoarding() {
                             onChange={handleChange}
                         />
 
-                        <label>Birthday</label>
-                        <div className="multiple-input-container">
-                            <input 
-                                id="dob_month"
-                                type="number"
-                                name="dob_month"
-                                placeholder="MM"
-                                required={true}
-                                value={formData.dob_month}
-                                onChange={handleChange}
-                            />
-                            <input 
-                                id="dob_day"
-                                type="number"
-                                name="dob_day"
-                                placeholder="DD"
-                                required={true}
-                                value={formData.dob_day}
-                                onChange={handleChange}
-                            />
-                            <input 
-                                id="dob_year"
-                                type="number"
-                                name="dob_year"
-                                placeholder="YYYY"
-                                required={true}
-                                value={formData.dob_year}
-                                onChange={handleChange}
-                            />
-                        </div>
-
-                        <label>Gender</label>
-                        <div className="multiple-input-container">
-                            <input 
-                                id="male-gender-identity"
-                                type="radio"
-                                name="gender_identity"
-                                value="Male"
-                                onChange={handleChange}
-                                checked={formData.gender_identity === 'Male'}
-                            />
-                            <label htmlFor="male-gender-identity">Male</label>
-                            <input 
-                                id="female-gender-identity"
-                                type="radio"
-                                name="gender_identity"
-                                value="Female"
-                                onChange={handleChange}
-                                checked={formData.gender_identity === 'Female'}
-                            />
-                            <label htmlFor="female-gender-identity">Female</label>
-                            <input 
-                                id="other-gender-identity"
-                                type="radio"
-                                name="gender_identity"
-                                value="Other"
-                                onChange={handleChange}
-                                checked={formData.gender_identity === 'Other'}
-                            />
-                            <label htmlFor="other-gender-identity">Other</label>
-                        </div>
-
-                        <label>Show Me</label>
-                        
-                        <div className="multiple-input-container">
-                            <input 
-                                id="male-gender-interest"
-                                type="radio"
-                                name="gender_interest"
-                                value="Male"
-                                onChange={handleChange}
-                                checked={formData.gender_interest === 'Male'}
-                            />
-                            <label htmlFor="male-gender-interest">Male</label>
-                            <input 
-                                id="female-gender-interest"
-                                type="radio"
-                                name="gender_interest"
-                                value="Female"
-                                onChange={handleChange}
-                                checked={formData.gender_interest === 'Female'}
-                            />
-                            <label htmlFor="female-gender-interest">Female</label>
-                            <input 
-                                id="everyone-gender-interest"
-                                type="radio"
-                                name="gender_interest"
-                                value="Everyone"
-                                onChange={handleChange}
-                                checked={formData.gender_interest === 'Everyone'}
-                            />
-                            <label htmlFor="everyone-gender-interest">Everyone</label>
-                        </div>
+                        <label htmlFor="pronouns">Last Name</label>
+                        <input 
+                            id="last_name"
+                            type="text"
+                            name="last_name"
+                            placeholder="Last Name"
+                            required={true}
+                            value={formData.last_name}
+                            onChange={handleChange}
+                        />
 
                         <label htmlFor="about">About Me</label>
                         <input 
