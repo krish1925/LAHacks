3f42f6f0678b8965dfa68659898eddc0bc40f985
preyasigaur <preyasi4203@gmail.com>
2023-04-23 00:16:07 -0700

    navbar style

--- frontend/happytravels/src/components/Nav.js
+++ frontend/happytravels/src/components/Nav.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 // import { useCookies } from 'react-cookie';
+import "./Nav.css"
 import apiClient from "../apiClient";
 
 function Nav() {
