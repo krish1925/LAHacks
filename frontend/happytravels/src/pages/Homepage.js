4f24681b7d5fdc5f5a393595c3b68b06cd726b26
nemi desai <nemi@s-169-232-84-175.resnet.ucla.edu>
2023-04-23 09:06:57 -0700

    buttons on home page

--- frontend/happytravels/src/pages/Homepage.js
+++ frontend/happytravels/src/pages/Homepage.js
@@ -7,14 +7,37 @@ function Homepage() {
   const [inputValue, setInputValue] = useState("");
   const navigate = useNavigate();
 
+  const [formData, setFormData] = useState ({
+    // user_id: cookies.UserId,
+    category: ''
+})
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate("/feed", { state: { location: inputValue } });
+    if (formData.category === 'Feed') {
+      navigate("/feed", { state: { location: inputValue } });
+    }
+    if (formData.category === 'Itinerary') {
+      navigate("/Itinenary", { state: { location: inputValue } });
+    }
+    if (formData.category === 'Safety/Tips') {
+      navigate("/safety", { state: { location: inputValue } });
+    }
   };
   
   const onChangeHandler = (e) => {
     setInputValue(e.target.value);
   }
+
+  function handleChange(e) {
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    const name = e.target.name;
+     
+    setFormData ((prevState) => ({
+      ...prevState,
+      [name] : value
+  }))
+}
 
   return (
     <div>
@@ -31,6 +54,41 @@ function Homepage() {
             onChange={onChangeHandler}
           />
           <button type="submit">Go</button>
+
+          <br />
+          <label>Category</label>
+          <div className="multiple-input-container">
+              <input 
+                  id="feed"
+                  type="radio"
+                  name="category"
+                  value="Feed"
+                  onChange={handleChange}
+                  checked={formData.category === 'Feed'}
+              />
+                <label htmlFor="itinerary-category" style={{ marginLeft: '10px' }}>Feed</label>
+              <br />
+              <input 
+                  id="itinerary"
+                  type="radio"
+                  name="category"
+                  value="Itinerary"
+                  onChange={handleChange}
+                  checked={formData.category === 'Itinerary'}
+              />
+              <label htmlFor="itinerary-category" style={{ marginLeft: '10px' }}>Itinerary</label>
+              <br />
+              <input 
+                  id="safety"
+                  type="radio"
+                  name="category"
+                  value="Safety/Tips"
+                  onChange={handleChange}
+                  checked={formData.category === 'Safety/Tips'}
+              />
+                <label htmlFor="itinerary-category" style={{ marginLeft: '10px' }}>Safety</label>
+          </div>
+
         </form>
       </div>
     </div>
